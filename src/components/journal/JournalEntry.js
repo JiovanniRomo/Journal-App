0f0e4c6faa9858f9fe65c543ae02c23d18ee7798
Jiovanni <srr190802@gmail.com>
2020-11-05 11:44:52 -0600

    Ahora es posible activar la nota para su edición. Además podemos cambiar la nita activa al cambiar la seleccion

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 
 export const JournalEntry = ({ id, title, body, date, url_image }) => {
 
+    const dispatch = useDispatch();
+
     const noteDate = moment(date);
 
+    const handleEntryClick = () => {
+        dispatch(activeNote(id, {
+            title, body, date, url_image
+        }));
+    };
+
     return (
-        <div className="journal__entry pointer">
+        <div 
+            className="journal__entry pointer"
+            onClick={handleEntryClick}
+        >
 
 
             {
