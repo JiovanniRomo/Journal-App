8b95801326a2a689a2f3b423630fd1130a0f3aaa
Jiovanni Romo <srr190802@gmail.com>
2021-01-26 10:48:14 -0600

    Se cambio un comentario en la App principal

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -6,7 +6,7 @@ import { store } from './store/store';
 export const JournalApp = () => {
 
     //Al ser el punto más alto de nuestra app, es un HOC (el index no vale, dejalo limpio),
-    //así que aquí proveeremos el acceso a la store
+    //así que aquí proveeremos el acceso al store
 
     return (
         <div>
